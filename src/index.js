56b53601b9c5e90cd52f40e3c0551c1cf1e8e89d
Ekaterina1994 <katishteno@mail.ru>
2022-02-21 18:12:59 +0300

    feat: add function for count score

--- src/index.js
+++ src/index.js
@@ -1,75 +1,18 @@
 // import css  from './style.css';
-/*
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
-import { update as updateFood, draw as drawFood } from './food.js'
-import { outsideGrid } from './grid.js'
 
-let lastRenderTime = 0
-let gameOver = false
-const gameBoard = document.querySelector('.game-board')
-
-function main(currentTime) {
-  if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
-    }
-    return
-  }
-
-
-  window.requestAnimationFrame(main)
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
-
-
-  lastRenderTime = currentTime
-
-  update()
-  draw()
-}
-
-window.requestAnimationFrame(main)
-
-function update() {
-  updateSnake()
-  updateFood()
-  checkDeath()
-}
-
-function draw() {
-  gameBoard.innerHTML = ''
-  drawSnake(gameBoard)
-  drawFood(gameBoard)
-}
-
-function checkDeath() {
-  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
-}
-
-
-let score = 0;
-
-let altScore = function(score_val){
-   ele_score.innerHTML = String(score_val);
-}
-
-if(checkBlock(snake[0].x, snake[0].y, food.x, food.y)){
-   snake[snake.length] = {x: snake[0].x, y: snake[0].y};
-   score += 1;
-   altScore(score);
-   addFood();
-   activeDot(food.x, food.y);
-}
-
-ele_score = document.getElementById("score_value");
-
-*/
+const newGame = document.querySelector('.new-game');
+const records = document.querySelector('.records');
+const buttons = document.querySelector('.buttons');
+const results = document.querySelector('.game-over');
+const sc = document.querySelector('.records-score');
 
 let field = document.createElement('div');
-document.body.appendChild(field);
+let footer = document.querySelector('.footer');
+document.body.insertBefore(field, footer);
 field.classList.add('field');
 
-for (let i = 1; i < 101; i++) {
+
+for (let i = 1; i < 401; i++) {
    let excel = document.createElement('div');
    field.appendChild(excel);
    excel.classList.add('excel');
@@ -77,10 +20,10 @@ for (let i = 1; i < 101; i++) {
 
 let excel = document.getElementsByClassName('excel');
 let x = 1;
-let y = 10;
+let y = 20;
 
 for (let i = 0; i < excel.length; i++) {
-   if (x > 10) {
+   if (x > 20) {
       x = 1;
       y--;
    }
@@ -91,8 +34,8 @@ for (let i = 0; i < excel.length; i++) {
 
 
 function generateSnake() {
-   let posX = Math.round(Math.random() * (10 - 3) + 3);
-   let posY = Math.round(Math.random() * (10 - 1) + 1);
+   let posX = Math.round(Math.random() * (20 - 3) + 3);
+   let posY = Math.round(Math.random() * (20 - 1) + 1);
    return [posX, posY];
 }
 let coordinates = generateSnake();
@@ -108,8 +51,8 @@ let mouse;
 
 function createMouse() {
    function generateMouse() {
-      let posX = Math.round(Math.random() * (10 - 1) + 1);
-      let posY = Math.round(Math.random() * (10 - 1) + 1);
+      let posX = Math.round(Math.random() * (20 - 1) + 1);
+      let posY = Math.round(Math.random() * (20 - 1) + 1);
       return [posX, posY];
    }
    let mouseCoordinates = generateMouse();
@@ -126,14 +69,21 @@ createMouse();
 let direction = 'right';
 let steps = false;
 
-let input = document.createElement('input');
-document.body.appendChild(input);
-input.classList.add('input');
+let dom_score = document.querySelector("#score_value");
+let dom_score2 = document.querySelector('#score_val');
+let score = 00;
+let maxScore = window.localStorage.getItem("maxScore") || undefined;
 
-let score = 0;
-input.value = `Your scores: ${score}`;
+let isPlay = false;
 
+const audio = new Audio();
 
+function playAudio() {
+   audio.src = './assets/audio/snakeAttack.mp3';
+   audio.currentTime = 0;
+   audio.play();
+  
+}
 
 function move() {
    let snakeCoordinates = [snakeBody[0].getAttribute('posX'), snakeBody[0].getAttribute('posY')];
@@ -142,7 +92,7 @@ function move() {
    snakeBody.pop();
 
    if (direction == 'right') {
-      if (snakeCoordinates[0] < 10) {
+      if (snakeCoordinates[0] < 20) {
          snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
       } else {
          snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
@@ -151,10 +101,10 @@ function move() {
       if (snakeCoordinates[0] > 1) {
          snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
       } else {
-         snakeBody.unshift(document.querySelector('[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'));
+         snakeBody.unshift(document.querySelector('[posX = "20"][posY = "' + snakeCoordinates[1] + '"]'));
       }
    } else if (direction == 'up') {
-      if (snakeCoordinates[1] < 10) {
+      if (snakeCoordinates[1] < 20) {
          snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1] + 1) + '"]'));
       } else {
          snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'));
@@ -163,7 +113,7 @@ function move() {
       if (snakeCoordinates[1] > 1) {
          snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1] - 1) + '"]'));
       } else {
-         snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "10"]'));
+         snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "20"]'));
       }
    }
 
@@ -173,12 +123,27 @@ function move() {
       let b = snakeBody[snakeBody.length - 1].getAttribute('posY');
       snakeBody.push(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
       createMouse();
-      score++;
-      input.value = `Your scores: ${score}`;
+      incrementScore();
+      playAudio();
    }
 
+   function incrementScore() {
+      score++;
+      dom_score.innerText = score.toString().padStart(2, "0");
+      dom_score2.innerText = score.toString().padStart(2, "0");
+   }
+
+
    if (snakeBody[0].classList.contains('snakeBody')) {
-      alert('Game over');
+      field.classList.add('hide');
+      results.classList.remove('hide');
+      results.classList.add('show');
+      for (let i = 0; i < snakeBody.length; i++) {
+         snakeBody[i].classList.remove('snakeBody');
+      }
+      snakeBody[0].classList.remove('head');
+      dom_score2.innerText = score.toString().padStart(2, "0");
+      snakeBody = [document.querySelector('[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'), document.querySelector('[posX = "' + (coordinates[0] - 1) + '"][posY = "' + coordinates[1] + '"]'), document.querySelector('[posX = "' + (coordinates[0] - 2) + '"][posY = "' + coordinates[1] + '"]')];
       clearInterval(interval);
    }
       
@@ -190,7 +155,7 @@ function move() {
    }
 
 
-let interval = setInterval(move, 400);
+let interval;
 
 window.addEventListener('keydown', function(e) {
    if (steps == true) {
@@ -209,3 +174,67 @@ window.addEventListener('keydown', function(e) {
       }
    }
 });
+
+
+function gameOver() {
+   maxScore ? null : (maxScore = score);
+   score > maxScore ? (maxScore = score) : null;
+   window.localStorage.setItem("maxScore", maxScore);
+}
+
+function reset() {
+  dom_score.innerText = "00";
+  dom_score2.innerText = "00";
+  score = "00";
+}
+
+
+
+newGame.addEventListener('click', startGame);
+function startGame() {
+   buttons.classList.add('hide');
+   field.classList.remove('hide');
+   field.classList.add('show');
+   interval = setInterval(move, 300);
+}
+
+// сохранение score
+let allScore = document.querySelectorAll(".score-text");
+let countScore = 0;
+function saveScore(score) {
+  if (countScore <= 10) {
+    allScore[countScore].innerHTML = `Game ${countScore + 1}: ${score}`;
+    countScore++;
+  }
+}
+
+
+records.addEventListener('click', showRecords);
+
+function showRecords() {
+   buttons.classList.add('hide');
+   sc.classList.add('show');
+   saveScore(dom_score.innerText = score.toString().padStart(2, "0"));
+}
+
+
+
+const menu = document.querySelector('.back-to-menu');
+menu.addEventListener('click', changeScreen);
+const menu2 = document.querySelector('.back-to-menu2');
+menu2.addEventListener('click', changeScreen);
+
+function changeScreen() {
+   records.classList.remove('hide');
+   buttons.classList.remove('hide');
+   results.classList.add('hide');
+   sc.classList.remove('show');
+}
+
+
+
+
+
+
+
+
